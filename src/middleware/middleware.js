2b486b6c3c4ada6adfaf2d89e6aa1f1fbf5c3fb1
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-02-02 17:38:19 -0300

    feat/Pegando o body da requisição

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,4 +1,15 @@
 export async function Middleware(req, res) {
+    const buff = [];
+
+    for await (const chunk of req) {
+        buff.push(Buffer.from(String(chunk)));
+    }
+
+    try {
+        req.body = JSON.parse(Buffer.concat(buff).toString());
+    } catch {
+        req.body = null;
+    }
 
     res.setHeader('Content-type', 'application/json');
 }
