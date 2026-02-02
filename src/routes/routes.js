8aefac3db958114aaeb4836f4f4d3f09f0058911
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-02-02 17:21:43 -0300

    feat/Criando estrutura da rota de criar task

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,14 @@ import { DataBase } from "../database.js"
 const database = new DataBase()
 
 export const Routes = [
-    {},
+    {
+        method: 'POST',
+        url: '/task',
+        handler: (req, res) => {
+
+            res.writeHead(201).end("Usuário criado com sucesso!")
+        }
+    },
     {},
     {}
 ]
