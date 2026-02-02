9800aff6a4caf2f968b860347775b6d91a1f1d5b
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-02-02 17:38:19 -0300

    feat/Pegando o body da requisição

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,7 @@ export const Routes = [
         method: 'POST',
         url: '/task',
         handler: (req, res) => {
+            console.log(req.body)
 
             res.writeHead(201).end("Usuário criado com sucesso!")
         }
