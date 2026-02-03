ca5296340140a4464bb267e7431ba7e353c4e625
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-02-02 21:04:21 -0300

    feat: Criando método para deletar uma task

--- src/database.js
+++ src/database.js
@@ -33,4 +33,23 @@ export class DataBase {
 
         this.#persist();
     }
+
+    delete(table, id) {
+        // 1 - Tabela e o id
+        // 2 - Verificar se o id está no array, se tiver apagar
+        // 3 - Verificar se o id está no array
+        // 4 - a task com o id removido
+
+        // validar para ver se o id está no array - x
+        // Se tiver apaga, se não tiver retornar um erro - x
+
+        const index = this.#database[table].findIndex(i => i.id === id);
+
+        if(index > -1) {
+            this.#database[table].splice(index, 1);
+            return true;
+        }
+
+        return false;
+    }
 }
